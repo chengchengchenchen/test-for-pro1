4b9a762280ede94cc0fc37c9d7861f45f220532a
chengchengchenchen <772809505@qq.com>
2023-03-31 16:27:04 +0800

    d

--- pro1/ContentView.swift
+++ pro1/ContentView.swift
@@ -7,72 +7,106 @@
 
 import SwiftUI
 
+struct Album{
+    var name:String
+    var singer:String
+    var text:String
+    var describe: String
+    var time:Int
+}
+let albums = [
+    Album(name:"mojito", singer: "Jzhou", text:"test", describe:"best for you", time: 190),
+    Album(name:"ed", singer: "", text:"test", describe:"best for you", time: 200),
+    Album(name:"hybrid theory", singer: "Jzhou", text:"test", describe:"best for you", time: 190),
+    Album(name:"linkin park1", singer: "Jzhou", text:"test", describe:"best for you", time: 190),
+    Album(name:"taylor", singer: "Jzhou", text:"test", describe:"best for you", time: 190),
+    Album(name:"rise", singer: "Jzhou", text:"test", describe:"best for you", time: 190),
+    Album(name:"ignite", singer: "Jzhou", text:"test", describe:"best for you", time: 190)
+]
 struct ContentView: View {
     var body: some View {
-        TabView{
-            listen()
-                .tabItem({
-                    Group{
-                        Text("listen now")
-                        Image(systemName: "arrowtriangle.right.circle")
-                    }
-                })
-            browse()
-                .tabItem({
-                    Group{
-                        Text("browse")
-                        Image(systemName: "square.split.2x2.fill")
-                    }
-                })
-            radio()
-                .tabItem({
-                    Group{
-                        Text("radio")
-                        Image(systemName: "dot.radiowaves.left.and.right")
-                    }
-                })
-            library()
-                .tabItem({
-                    Group{
-                        Text("library")
-                        Image(systemName: "play.square.stack.fill")
-                    }
-                })
-            search()
-                .tabItem({
-                    Group{
-                        Text("search")
-                        Image(systemName: "magnifyingglass")
-                    }
-                })
-        }.accentColor(.red)
+        ZStack(alignment: .bottom){
+            TabView{
+                listen()
+                    .tabItem({
+                        Group{
+                            Text("listen now")
+                            Image(systemName: "arrowtriangle.right.circle")
+                        }
+                    })
+                browse()
+                    .tabItem({
+                        Group{
+                            Text("browse")
+                            Image(systemName: "square.split.2x2.fill")
+                        }
+                    })
+                radio()
+                    .tabItem({
+                        Group{
+                            Text("radio")
+                            Image(systemName: "dot.radiowaves.left.and.right")
+                        }
+                    })
+                library()
+                    .tabItem({
+                        Group{
+                            Text("library")
+                            Image(systemName: "play.square.stack.fill")
+                        }
+                    })
+                search()
+                    .tabItem({
+                        Group{
+                            Text("search")
+                            Image(systemName: "magnifyingglass")
+                        }
+                    })
+            }.accentColor(.red)
+            
+            VStack{
+                Divider()
+                HStack {
+                    Image(systemName: "play.circle.fill")
+                    Text("Now playing")
+                    Spacer()
+                    Image(systemName: "forward.fill")
+                }
+                Divider()
+            }.background(Rectangle()
+                .fill(Color.white)
+                .frame(height: 20))
+            .offset(y:-60)
+        }
     }
 }
-struct pictureText: View{
-    var pictureName=""
-    var text=""
-    let wsize=180.0
+struct pCard: View{
+    var album:Album
+    var wsize=200.0
     var body: some View{
-        ZStack(alignment:.bottom){
-            Image(pictureName)
-                .resizable()
-                .cornerRadius(10.0)
-                //.frame(width: wsize,height: wsize)
-            
-            Text(text)
+        VStack(alignment: .leading){
+            Text(album.describe)
                 .font(.body)
-                .foregroundColor(.white)
-                .multilineTextAlignment(.center)
-                .padding(.all, 7.0)
-                .frame(width:wsize)
-                .background(
-                    .ultraThinMaterial
-                        .opacity(0.9)
-                )
-                .cornerRadius(10.0)
+                .foregroundColor(.gray)
+            
+            ZStack(alignment:.bottom){
+                Image(album.name)
+                    .resizable()
+                    .cornerRadius(10.0)
+                
+                Text(album.text)
+                    .font(.body)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.all, 7.0)
+                    .frame(width:wsize)
+                    .background(.ultraThinMaterial.opacity(0.9))
+                    .cornerRadius(10.0)
+            }
+            .frame(width: wsize,height: wsize*1.5)
         }
-        .frame(width: wsize,height: wsize*1.5)
     }
+    
 }
 
 struct listen: View{
@@ -82,11 +116,17 @@ struct listen: View{
                 Text("Listen Now")
                     .font(.largeTitle)
                     .padding(.leading, 20.0)
-
+                
                 Divider()
                 ScrollView(.horizontal,showsIndicators: false){
-                    HStack{
-                        Text("tset")
+                    HStack(spacing:5){
+                        Spacer()
+                        pCard(album:albums[0])
+                        Spacer()
+                        pCard(album:albums[1])
+                        Spacer()
+                        pCard(album:albums[2])
+                        Spacer()
                     }
                 }
             }
